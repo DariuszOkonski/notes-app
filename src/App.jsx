3bd10a5f647ca205bd684dd7ee2e960cfb169768
Dariusz Okoński <darek200180@gmail.com>
2026-01-29 19:33:12 +0100

    add NoteList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react';
 import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
 
 function App() {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: 'first title',
+      priority: 'Medium',
+      category: 'Work',
+      description: 'Some first description',
+    },
+    {
+      id: 2,
+      title: 'second title',
+      priority: 'Medium',
+      category: 'Work',
+      description: 'Some second description',
+    },
+  ]);
 
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'>
       <h2 className='text-2xl font-bold mb-4 text-center'>Notes App</h2>
 
       <NoteForm notes={notes} setNotes={setNotes} />
+
+      <NoteList notes={notes} />
     </div>
   );
 }
