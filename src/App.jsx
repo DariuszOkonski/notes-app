99b3cfdffd342d793234cadb704a0e2e58d9719f
Dariusz Okoński <darek200180@gmail.com>
2026-01-29 20:10:04 +0100

    add delition option

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ import NoteList from './components/NoteList';
 
 function App() {
   const [notes, setNotes] = useState([
-    // {
-    //   id: 1,
-    //   title: 'first title',
-    //   priority: 'Medium',
-    //   category: 'Work',
-    //   description: 'Some first description',
-    // },
-    // {
-    //   id: 2,
-    //   title: 'second title',
-    //   priority: 'Medium',
-    //   category: 'Work',
-    //   description: 'Some second description',
-    // },
+    {
+      id: 1,
+      title: 'first title',
+      priority: 'Medium',
+      category: 'Work',
+      description: 'Some first description',
+    },
+    {
+      id: 2,
+      title: 'second title',
+      priority: 'Medium',
+      category: 'Work',
+      description: 'Some second description',
+    },
+    {
+      id: 3,
+      title: 'third title',
+      priority: 'Medium',
+      category: 'Work',
+      description: 'Some third second description',
+    },
   ]);
+
+  const handleDelete = (id) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
 
   return (
     <div className='max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg'>
@@ -26,7 +37,7 @@ function App() {
 
       <NoteForm notes={notes} setNotes={setNotes} />
 
-      <NoteList notes={notes} />
+      <NoteList notes={notes} deleteNote={handleDelete} />
     </div>
   );
 }
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,4 +1,4 @@
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, deleteNote }) => {
   if (notes.length === 0) {
     return <p className='text-center text-gray-500'>No Notes Yet</p>;
   }
@@ -20,6 +20,12 @@ const NoteList = ({ notes }) => {
             {note.priority}
           </p>
           <p className='mt-2'>{note.description}</p>
+          <button
+            className='mt-3 text-red-500 bg-gray-200 p-2 rounded-lg cursor-pointer hover:text-red-700'
+            onClick={() => deleteNote(note.id)}
+          >
+            Delete
+          </button>
         </div>
       ))}
     </div>
