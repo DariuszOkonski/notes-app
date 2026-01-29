8bb5b850d2d41acf8e33cb127a4989ef52053877
Dariusz Okoński <darek200180@gmail.com>
2026-01-29 19:41:44 +0100

    add functionality to make form visible or hidden

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 const defaultState = {
   title: '',
@@ -9,6 +9,7 @@ const defaultState = {
 
 const NoteForm = ({ notes, setNotes }) => {
   const [formData, setFormData] = useState(defaultState);
+  const [isFormVisible, setIsFormVisible] = useState(false);
 
   const handleChange = (e) => {
     setFormData((prevState) => ({
@@ -35,71 +36,82 @@ const NoteForm = ({ notes, setNotes }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className='mb-6'>
-      <div className='mb-4'>
-        <label htmlFor='title' className='block font-semibold'>
-          Title
-        </label>
-        <input
-          type='text'
-          name='title'
-          className='w-full p-2 border rounded-lg'
-          value={formData.title}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className='mb-4'>
-        <label htmlFor='priority' className='block font-semibold'>
-          Priority
-        </label>
-        <select
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          name='priority'
-          value={formData.priority}
-          onChange={handleChange}
-        >
-          <option value='High'>High</option>
-          <option value='Medium'>Medium</option>
-          <option value='Low'>Low</option>
-        </select>
-      </div>
-
-      <div className='mb-4'>
-        <label htmlFor='category' className='block font-semibold'>
-          Category
-        </label>
-        <select
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          name='category'
-          value={formData.category}
-          onChange={handleChange}
-        >
-          <option value='Work'>Work</option>
-          <option value='Personal'>Personal</option>
-          <option value='Ideas'>Ideas</option>
-        </select>
-      </div>
-
-      <div className='mb-4'>
-        <label htmlFor='description' className='block font-semibold'>
-          Description
-        </label>
-        <textarea
-          type='text'
-          className='w-full p-2 border rounded-lg'
-          name='description'
-          value={formData.description}
-          onChange={handleChange}
-        ></textarea>
-      </div>
-
-      <button className='w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600'>
-        Add Note
+    <React.Fragment>
+      <button
+        className='w-full bg-gray-100 border border-gray-300 text-purple-800 py-2 rounded-lg cursor-pointer hover:bg-purple-200 hover:border-purple-300 transition mb-4'
+        onClick={() => setIsFormVisible((prevState) => !prevState)}
+      >
+        {isFormVisible ? 'Hide Form' : 'Add New Note'}
       </button>
-    </form>
+
+      {isFormVisible && (
+        <form onSubmit={handleSubmit} className='mb-6'>
+          <div className='mb-4'>
+            <label htmlFor='title' className='block font-semibold'>
+              Title
+            </label>
+            <input
+              type='text'
+              name='title'
+              className='w-full p-2 border rounded-lg'
+              value={formData.title}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className='mb-4'>
+            <label htmlFor='priority' className='block font-semibold'>
+              Priority
+            </label>
+            <select
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              name='priority'
+              value={formData.priority}
+              onChange={handleChange}
+            >
+              <option value='High'>High</option>
+              <option value='Medium'>Medium</option>
+              <option value='Low'>Low</option>
+            </select>
+          </div>
+
+          <div className='mb-4'>
+            <label htmlFor='category' className='block font-semibold'>
+              Category
+            </label>
+            <select
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              name='category'
+              value={formData.category}
+              onChange={handleChange}
+            >
+              <option value='Work'>Work</option>
+              <option value='Personal'>Personal</option>
+              <option value='Ideas'>Ideas</option>
+            </select>
+          </div>
+
+          <div className='mb-4'>
+            <label htmlFor='description' className='block font-semibold'>
+              Description
+            </label>
+            <textarea
+              type='text'
+              className='w-full p-2 border rounded-lg'
+              name='description'
+              value={formData.description}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+
+          <button className='w-full bg-purple-500 text-white py-2 rounded-lg cursor-pointer hover:bg-purple-600'>
+            Add Note
+          </button>
+        </form>
+      )}
+    </React.Fragment>
   );
 };
 
