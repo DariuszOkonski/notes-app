4cf203b961a51ba8d17e847729e464b31de3f4a7
Dariusz Okoński <darek200180@gmail.com>
2026-01-29 19:58:09 +0100

    add SelectInput

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TextInput from './inputs/TextInput';
+import SelectInput from './inputs/SelectInput';
 
 const defaultState = {
   title: '',
@@ -54,39 +55,29 @@ const NoteForm = ({ notes, setNotes }) => {
             onChange={handleChange}
           />
 
-          <div className='mb-4'>
-            <label htmlFor='priority' className='block font-semibold'>
-              Priority
-            </label>
-            <select
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              name='priority'
-              value={formData.priority}
-              onChange={handleChange}
-            >
-              <option value='High'>High</option>
-              <option value='Medium'>Medium</option>
-              <option value='Low'>Low</option>
-            </select>
-          </div>
+          <SelectInput
+            label='Priority'
+            name='priority'
+            value={formData.priority}
+            onChange={handleChange}
+            options={[
+              { value: 'High', label: 'High' },
+              { value: 'Medium', label: 'Medium' },
+              { value: 'Low', label: 'Low' },
+            ]}
+          />
 
-          <div className='mb-4'>
-            <label htmlFor='category' className='block font-semibold'>
-              Category
-            </label>
-            <select
-              type='text'
-              className='w-full p-2 border rounded-lg'
-              name='category'
-              value={formData.category}
-              onChange={handleChange}
-            >
-              <option value='Work'>Work</option>
-              <option value='Personal'>Personal</option>
-              <option value='Ideas'>Ideas</option>
-            </select>
-          </div>
+          <SelectInput
+            label='Category'
+            name='category'
+            value={formData.category}
+            onChange={handleChange}
+            options={[
+              { value: 'Work', label: 'Work' },
+              { value: 'Personal', label: 'Personal' },
+              { value: 'Ideas', label: 'Ideas' },
+            ]}
+          />
 
           <div className='mb-4'>
             <label htmlFor='description' className='block font-semibold'>
